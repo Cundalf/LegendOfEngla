b618916148862cba2aff2f88ce5d55cd77c00aa5
Agustin Cundari <acundari95@gmail.com>
2020-07-15 23:02:11 -0300

    DialogTemp Added
    Tutorial Dialog

---
+++ Assets/Scripts/DialogTemp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTemp : MonoBehaviour
+{
+    void Start()
+    {
+        string[] dialog = {"Los controles estan en desarrollo. \n Te moves con las flechas del teclado. \n Con click atacas y usas la interfaz. \n Con click derecho ejecutas acciones." };
+        FindObjectOfType<DialogManager>().ShowDialogue(dialog);
+        gameObject.SetActive(false);
+    }
+}
