aa82c9ae621ef2e70bf01064ced8502f276b3935
Agustin Cundari <acundari95@gmail.com>
2020-07-17 16:21:27 -0300

    Cambios acumulados.
    Mejoras en sistemas de audio con un gestor de volumen.
    Se agrego un sistema basico de monedas virtuales.
    Se cambiaro algunos parametros de los ProjectSettings.

---
+++ Assets/Scripts/Money.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Money : MonoBehaviour
+{
+    public int value;
+    private MoneyManager manager;
+
+    void Start()
+    {
+        manager = FindObjectOfType<MoneyManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name.Equals("Player"))
+        {
+            manager.AddMoney(value);
+            Destroy(gameObject);
+        }
+    }
+}
